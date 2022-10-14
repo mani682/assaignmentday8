7e78339a3fc7c59869f9c2438ec84b4055af7e78
mani682 <manichandanamiriyala682@gmail.com>
2022-10-14 14:23:34 +0530

    employee wage

---
+++ uc1_employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace totalemployeewage
+{
+    internal  class uc1_employee
+    {
+        public void ToCheckEmpAttendence()
+        {
+            int Is_Fulltime = 1;
+            Random random = new Random();
+            int emplCheck = random.Next(0, 2);
+            if (emplCheck == 1)
+            {
+                Console.WriteLine("employee is present");
+            }
+            else
+            {
+                Console.WriteLine("employee is absent");
+            }
+        }
+    }
+}
