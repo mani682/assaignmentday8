22b714d012c8e89908f4d692f23fe5e2e7fbe451
mani682 <manichandanamiriyala682@gmail.com>
2022-10-14 14:24:20 +0530

    employeewage

--- Program.cs
+++ Program.cs
@@ -1,16 +1,12 @@
-﻿namespace employeewage
+﻿namespace totalemployeewage
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computataion Problem");
-            //uc1_employee.ToCheckEmpAttendenc
-            // uc2_employee.ToCheckCalWage();
-            //uc3_employee.ToCalculateEmpWage();
-            // uc4_employee.ToCalculateEmpWage();
-           // uc5_employee.EmployeeWage();
-           uc6_employee.TotalEmpWage();
+            uc1_employee employeeAttendence = new uc1_employee();
+            employeeAttendence.ToCheckEmpAttendence();
         }
     }
 }
